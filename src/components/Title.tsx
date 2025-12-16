df702c96450ccaf626bb4bdad66771547507f2b1
lucascampanharo <lucascampanharodev@gmail.com>
2025-12-16 13:59:07 -0300

    feat: Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+export default function Title() {
+  return (
+    <>
+      <section className="flex items-center justify-left">
+        <h1 className="text-5x1 md:text-7x1 font-bold text-center m-[50px]">
+          Campanharo <span className="text-blue-500">Lucas</span>
+        </h1>
+      </section>
+    </>
+  );
+}

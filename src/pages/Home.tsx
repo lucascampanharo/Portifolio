14e35847e9664a6c808b126a0e0ee48492aa63a6
lucascampanharo <lucascampanharodev@gmail.com>
2025-12-16 10:28:30 -0300

    init: inital portifolio setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <section className="min-h-screen felx items-center justify-center">
+        <h1 className="text-5x1 md:text-7x1 font-bold text-center">
+          Desenvolvedor <span className="text-blue-500">Frontend</span>
+        </h1>
+      </section>
+    </>
+  );
+}
+
+export default Home;

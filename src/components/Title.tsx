cbc510bea1bb03f950240e2f51d2f2cde3127209
lucascampanharo <lucascampanharodev@gmail.com>
2025-12-16 14:45:12 -0300

    feat: subtitle

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,13 @@
 export default function Title() {
   return (
-    <>
-      <section className="flex items-center justify-left">
-        <h1 className="text-5x1 md:text-7x1 font-bold text-center m-[50px]">
-          Campanharo <span className="text-blue-500">Lucas</span>
-        </h1>
-      </section>
-    </>
+    <section className="flex flex-col px-6 mt-8">
+      <h1 className="text-5xl md:text-7xl font-bold">
+        Campanharo <span className="text-blue-500">Lucas</span>
+      </h1>
+
+      <h2 className="mt-4 text-2xl md:text-3xl text-gray-400">
+        Desenvolvedor de Software
+      </h2>
+    </section>
   );
 }
